e6609809bcba5955ed348c5852ebd6f2e4ca39f6
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-06 20:11:12 +0200

    Chalk random color

--- index2.js
+++ index2.js
@@ -2,13 +2,15 @@ import { argv } from 'node:process';
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-//console.log(argv[2]);
+console.log(argv[2]);
 const rdnColor = randomColor();
-//if (argv[1]) {
-//}
+if (argv[1]) {
+  console.log(rdnColor);
+}
 
 const block = `#############################,#############################,#############################,#####                   #####,#####      ${rdnColor}      #####,#####                   #####,#############################,#############################,#############################`;
 
 //console.log(chalk.hex(randomColor())(block.split(',').join('\r\n')));
-const colorBlock = chalk.hex(randomColor())(block.split(',').join('\r\n'));
-console.log(colorBlock);
+const colorBlock = console.log(
+  chalk.hex(randomColor())(block.split(',').join('\r\n')),
+);

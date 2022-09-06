fb8d6216ab28c0834f44c07189f1aa42822e5382
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-06 19:44:54 +0200

    Update randomcolor

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import { argv } from 'node:process';
+import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const met = 12;
-console.log(met);
-const rdncol = randomColor();
-console.log(rdncol);
+//console.log(argv[2]);
+const rdnColor = randomColor();
+//if (argv[1]) {
+//console.log(rdnColor);
+//}
+
+const block = `#############################,#############################,#############################,#####                   #####,#####      ${rdnColor}      #####,#####                   #####,#############################,#############################,#############################`;
+
+console.log(chalk.hex(randomColor())(block.split(',').join('\r\n')));

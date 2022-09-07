e39bd43d0b7a99bee0fa77ceeb82f7779d576af9
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-07 08:07:29 +0200

    Update color input

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const block = `#############################,#############################,#####
 
 //console.log(chalk.hex(randomColor())(block.split(',').join('\r\n')));
 const colorBlock = chalk.hex(randomColor())(block.split(',').join('\r\n'));
-//console.log(colorBlock);
-
 const colorInput = { luminosity: 'light', hue: 'blue' };
 colorInput.hue = argv[2];
 colorInput.luminosity = argv[3];
 
 if (argv[1]) {
   console.log(colorBlock);
+}
+if (argv[2]) {
 }
